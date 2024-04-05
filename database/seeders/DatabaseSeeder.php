b4b0a9d5fdb6606c1117d6361fdd729e9c207f43
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-04-05 15:51:50 +0200

    Add test rates, options, alleys, and reservations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -32,60 +32,134 @@ class DatabaseSeeder extends Seeder
             'role_id' => '1',
             'password' => bcrypt('admin123'),
         ]);
-                // Create a test rate
-                Rate::create([
-                    'weekday' => 'Monday',
-                    'period' => 'Day',
-                    'rental_price' => 10,
-                ]);
-        
-                Option::create([
-                    'name' => 'Bowling',
-                    'description' => 'Bowling',
-                    'price' => 10,
-                ]);
-        
-                Alley::create([
-                    'alley_number' => 1,
-                    'has_bumpers' => 0,
-                ]);
-        
-                // Create 3 test reservations
-                WorkerReservation::create([
-                    'option_id' => 1,
-                    'rate_id' => 1,
-                    'alley_id' => 1,
-                    'user_name' => 'Test User',
-                    'user_phone' => '123456789',
-                    'date' => '2024-04-05 12:00:00',
-                    'duration' => 1,
-                    'total_cost' => 10,
-                    'amount_of_people' => 2,
-                    'amount_of_children' => 0,
-                ]);
-                WorkerReservation::create([
-                    'option_id' => 1,
-                    'rate_id' => 1,
-                    'Alley_id' => 1,
-                    'user_name' => 'Test User 2',
-                    'user_phone' => '123456789',
-                    'date' => '2024-04-05 14:00:00',
-                    'duration' => 1,
-                    'total_cost' => 10,
-                    'amount_of_people' => 2,
-                    'amount_of_children' => 0,
-                ]);
-                WorkerReservation::create([
-                    'option_id' => 1,
-                    'rate_id' => 1,
-                    'alley_id' => 1,
-                    'user_name' => 'Test User 3',
-                    'user_phone' => '123456789',
-                    'date' => '2024-04-05 16:00:00',
-                    'duration' => 1,
-                    'total_cost' => 10,
-                    'amount_of_people' => 2,
-                    'amount_of_children' => 0,
-                ]);
-            }
-        }
+
+        // Create a test rate
+        Rate::create([
+            'weekday' => 'Monday',
+            'period' => 'Day',
+            'rental_price' => 24,
+        ]);
+        Rate::create([
+            'weekday' => 'Monday',
+            'period' => 'Night',
+            'rental_price' => 24,
+        ]);
+        Rate::create([
+            'weekday' => 'Tuesday',
+            'period' => 'Day',
+            'rental_price' => 24,
+        ]);
+        Rate::create([
+            'weekday' => 'Tuesday',
+            'period' => 'Night',
+            'rental_price' => 24,
+        ]);
+        Rate::create([
+            'weekday' => 'Wednesday',
+            'period' => 'Day',
+            'rental_price' => 24,
+        ]);
+        Rate::create([
+            'weekday' => 'Wednesday',
+            'period' => 'Night',
+            'rental_price' => 24,
+        ]);
+        Rate::create([
+            'weekday' => 'Thursday',
+            'period' => 'Day',
+            'rental_price' => 24,
+        ]);
+        Rate::create([
+            'weekday' => 'Thursday',
+            'period' => 'Night',
+            'rental_price' => 24,
+        ]);
+        Rate::create([
+            'weekday' => 'Friday',
+            'period' => 'Day',
+            'rental_price' => 28,
+        ]);
+        Rate::create([
+            'weekday' => 'Friday',
+            'period' => 'Night',
+            'rental_price' => 33.50,
+        ]);
+        Rate::create([
+            'weekday' => 'Saturday',
+            'period' => 'Day',
+            'rental_price' => 28,
+        ]);
+        Rate::create([
+            'weekday' => 'Saturday',
+            'period' => 'Night',
+            'rental_price' => 33.50,
+        ]);
+        Rate::create([
+            'weekday' => 'Sunday',
+            'period' => 'Day',
+            'rental_price' => 28,
+        ]);
+        Rate::create([
+            'weekday' => 'Sunday',
+            'period' => 'Night',
+            'rental_price' => 33.50,
+        ]);
+
+        Option::create([
+            'name' => 'Bowling',
+            'description' => 'Bowling',
+            'price' => 10,
+        ]);
+
+        Alley::create([
+            'alley_number' => 1,
+            'has_bumpers' => 0,
+        ]);
+        Alley::create([
+            'alley_number' => 2,
+            'has_bumpers' => 1,
+        ]);
+        Alley::create([
+            'alley_number' => 3,
+            'has_bumpers' => 0,
+        ]);
+
+        // Create 3 test reservations
+        WorkerReservation::create([
+            'option_id' => 1,
+            'rate_id' => 1,
+            'alley_id' => 1,
+            'user_name' => 'Test User',
+            'user_phone' => '123456789',
+            'date' => '2024-04-05 12:00:00',
+            'duration' => 1,
+            'total_cost' => 10,
+            'amount_of_people' => 2,
+            'amount_of_children' => 0,
+        ]);
+        WorkerReservation::create([
+            'option_id' => 1,
+            'rate_id' => 1,
+            'Alley_id' => 1,
+            'user_name' => 'Test User 2',
+            'user_phone' => '123456789',
+            'date' => '2024-04-05 14:00:00',
+            'duration' => 1,
+            'total_cost' => 10,
+            'amount_of_people' => 2,
+            'amount_of_children' => 0,
+        ]);
+        WorkerReservation::create([
+            'option_id' => 1,
+            'rate_id' => 1,
+            'alley_id' => 1,
+            'user_name' => 'Test User 3',
+            'user_phone' => '123456789',
+            'date' => '2024-04-05 16:00:00',
+            'duration' => 1,
+            'total_cost' => 10,
+            'amount_of_people' => 2,
+            'amount_of_children' => 0,
+        ]);
+    }
+}
