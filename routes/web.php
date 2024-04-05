f478e364e4c353161d83b092ab2267456840f1ee
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2024-04-05 12:19:31 +0200

    route to the main score page and addscore

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ScoreController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,6 +11,16 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/scores', function () {
+    return view('scores');
+})->middleware(['auth', 'verified'])->name('scores');
+
+
+Route::get('/addscore', [ScoreController::class, 'index',])->middleware(['auth', 'verified'])->name('addscore.index');
+
+Route::post('/addscore', [ScoreController::class, 'store'])->middleware(['auth', 'verified'])->name('addscore.store');
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
