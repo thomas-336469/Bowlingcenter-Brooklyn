030bdc264c24f49344a6c265c32c946a3255ba2a
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-04-05 15:42:07 +0200

    Add routes for worker reservation management

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Middleware\AdminMiddleware;
 use App\Http\Controllers\AdminOptionsController;
 use Illuminate\Routing\Middleware;
 use App\Http\Controllers\UserReservationController;
+use App\Http\Controllers\WorkerReservationController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,6 +22,9 @@ Route::get('/admin/options', [AdminOptionsController::class, 'index'])->name('ad
 Route::middleware('auth')->group(function () {
     Route::get('/reservations', [UserReservationController::class, 'index'])->name('reservations.index');
     Route::get('/reservations/create', [UserReservationController::class, 'create'])->name('reservations.create');
+    Route::get('/reservations/$reservation/{id}/edit', [UserReservationController::class, 'edit'])->name('reservations.edit');
+    Route::patch('/reservations/$reservation/{id}update', [UserReservationController::class, 'update'])->name('reservations.update');
+    Route::delete('/reservations/$reservation/{id}/delete', [UserReservationController::class, 'destroy'])->name('reservations.destroy');
     Route::post('/reservations/store', [UserReservationController::class, 'store'])->name('reservations.store');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
