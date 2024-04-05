3d26740072c23a7833db538fd6073b66509b587d
ThePope05 <swobben05@gmail.com>
2024-04-05 09:46:53 +0200

    Update DatabaseSeeder with admin account

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,10 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'AdminAccount',
+            'email' => 'admin@bowling.com',
+            'role' => 'admin',
+            'password' => bcrypt('admin123'),
         ]);
     }
 }
