dacd92d9db86e7ef3f8bb864976e4a3cffda2425
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2024-04-05 13:41:20 +0200

    Added a delete button and function

--- resources/views/scores.blade.php
+++ resources/views/scores.blade.php
@@ -6,7 +6,21 @@
     </x-slot>
 
     <div class="bg-[#fbede2] py-5">
-        {!! $score !!}
+        @foreach ($score as $scoreOutput)
+            <div class='py-1 m-5 bg-[#fbd158]'>
+                <div class='flex gap-10 m-3 w-l'>
+                    <p class='align-self-center'>Naam: {{ $scoreOutput->name }} | Score: {{ $scoreOutput->score }}</p>
+                    <div>
+                        <button class='bg-[#fbede2] py-3 w-20 rounded-md'>Edit</button>
+                        <form method='POST' action=" {{ route('scores.delete', $scoreOutput->id) }}">
+                            @csrf
+                            @method('DELETE')
+                            <button type='submit' class='bg-[#fbede2] py-3 w-20 rounded-md'>Delete</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
     <button class="bg-[#53435e] py-3 w-20 rounded-md text-white">
         <a href="/addscore">
