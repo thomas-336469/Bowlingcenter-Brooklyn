5e878ef95bfc0c2b07768c5412e6ad0a1ed33112
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2024-04-05 12:19:07 +0200

    Add Score form added

---
+++ resources/views/addscore.blade.php
@@ -0,0 +1,31 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Scores') }}
+        </h2>
+    </x-slot>
+
+    <div class="card">
+        <div class="title">
+            <h1>
+                {{-- {{ $title }} --}}
+            </h1>
+        </div>
+        <form method="post" action="{{ route('addscore.store') }}">
+            @csrf
+            @method('POST')
+
+            <input type="hidden" name="reservation_id" value="1">
+
+
+            <label for="name">Name</label>
+            <input type="text" name="name" id="name" required><br>
+            <label for="score">Score</label>
+            <input type="number" name="score" id="score" required>
+
+            <input type="submit" value="Add score" id="add">
+        </form>
+
+    </div>
+
+</x-app-layout>
