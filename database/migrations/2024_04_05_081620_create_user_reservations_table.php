6bc8326f81f2d13e10d5e946a602d80dfacc8f78
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-04-05 14:14:30 +0200

    Add foreign key constraints and columns to user_reservations table

--- database/migrations/2024_04_05_081620_create_user_reservations_table.php
+++ database/migrations/2024_04_05_081620_create_user_reservations_table.php
@@ -13,15 +13,15 @@ return new class extends Migration
     {
         Schema::create('user_reservations', function (Blueprint $table) {
             $table->id();
-            // $table->foreignId('user_id')->constrained();
-            // $table->foreignId('rate_id')->constrained();
-            // $table->foreignId('alley_id')->constrained();
-            $table->text('option');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('rate_id')->constrained();
+            $table->foreignId('alley_id')->constrained();
+            $table->foreignId('option_id')->nullable()->constrained();
             $table->dateTime('date');
             $table->integer('duration');
-            // $table->decimal('total_cost', 5, 2);
+            $table->decimal('total_cost', 5, 2);
             $table->integer('amount_of_people');
-            // $table->integer('amount_of_children');
+            $table->integer('amount_of_children');
             $table->timestamps();
         });
     }
