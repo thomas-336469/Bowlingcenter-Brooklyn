e1ca6773651fe133c793170f51d2245564ad4bf8
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2024-04-05 13:05:38 +0200

    Update score view to dynamically display scores

--- resources/views/scores.blade.php
+++ resources/views/scores.blade.php
@@ -6,15 +6,7 @@
     </x-slot>
 
     <div class="bg-[#fbede2] py-5">
-        <div class="py-1 m-5 bg-[#fbd158]">
-            <div class="flex gap-10 m-3 w-l">
-                <p class="align-self-center">Naam: John | Score: 63</p>
-                <div>
-                    <button class="bg-[#fbede2] py-3 w-20 rounded-md">Edit</button>
-                    <button class="bg-[#fbede2] py-3 w-20 rounded-md">Delete</button>
-                </div>
-            </div>
-        </div>
+        {!! $score !!}
     </div>
     <button class="bg-[#53435e] py-3 w-20 rounded-md text-white">
         <a href="/addscore">
