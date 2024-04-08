6615071dae2af0dc1693134e6028493db2df5e30
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-04-08 13:24:46 +0200

    Refactor BaanReservationController to include user authentication and update index view

--- app/Http/Controllers/BaanReservationController.php
+++ app/Http/Controllers/BaanReservationController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\MazinReservation;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BaanReservationController extends Controller
 {
     public function index() 
     {
-        $mazinReservation = MazinReservation::all();
-        return view('baanReservation.index', compact('mazinReservation'));
+        $user = Auth::user(); // Get the authenticated user
+        $mazinReservations = MazinReservation::all();
+        return view('baanReservation.index', compact('mazinReservations', 'user'));
     }
 }
