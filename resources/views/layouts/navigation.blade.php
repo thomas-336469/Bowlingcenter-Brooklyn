725c3ddf873bbf4d23d5225690e698c54f6bf474
Tjardo Ihrig <tjardoihrig.zakelijk@gmail.com>
2024-04-08 10:40:15 +0200

    Navigation Fix

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -19,35 +19,23 @@
                                     {{ __('Dashboard') }}
                                 </x-nav-link>
                                 <!-- Conditionally show the link for users with an admin role -->
-                                @if(auth()->user()->role_id === 1)
-                                <x-nav-link class="text-dark" :href="route('admin.options.index')" :active="request()->routeIs('admin.options.index')">
-                                    {{ __('Options overzicht') }}
-                                </x-nav-link>
+                                @if (auth()->user()->role_id === 1)
+                                    <x-nav-link class="text-dark" :href="route('admin.options.index')" :active="request()->routeIs('admin.options.index')">
+                                        {{ __('Options overzicht') }}
+                                    </x-nav-link>
                                 @endif
                                 <x-nav-link class="text-dark" :href="route('scores')" :active="request()->routeIs('scores')">
                                     {{ __('Scores') }}
                                 </x-nav-link>
+                                <!-- Add a link to the reservations index page -->
+                                <x-nav-link :href="route('reservations.index')" :active="request()->routeIs('reservations.index')">
+                                    {{ __('Reservations') }}
+                                </x-nav-link>
+                                <!-- Add a link to the reservations create page -->
+                                <x-nav-link :href="route('reservations.create')" :active="request()->routeIs('reservations.create')">
+                                    {{ __('Create Reservation') }}
+                                </x-nav-link>
                             </div>
-                    </div>
-                    <!-- Navigation Links -->
-                    <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                            {{ __('Dashboard') }}
-                        </x-nav-link>
-                        <!-- Add a link to the reservations index page -->
-                        <x-nav-link :href="route('reservations.index')" :active="request()->routeIs('reservations.index')">
-                            {{ __('Reservations') }}
-                        </x-nav-link>
-                        <!-- Add a link to the reservations create page -->
-                        <x-nav-link :href="route('reservations.create')" :active="request()->routeIs('reservations.create')">
-                            {{ __('Create Reservation') }}
-                        </x-nav-link>
-                        <!-- Conditionally show the link for users with an admin role -->
-                        @if(auth()->user()->role_id === 1)
-                        <x-nav-link :href="route('admin.options.index')" :active="request()->routeIs('admin.options.index')">
-                            {{ __('Admin Options') }}
-                        </x-nav-link>
-                        @endif
                     </div>
                 </div>
 
@@ -55,12 +43,16 @@
                 <div class=" bg-main hidden sm:flex sm:items-center sm:ms-6">
                     <x-dropdown align="right" width="48">
                         <x-slot name="trigger">
-                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-xl leading-4 font-medium rounded-md text-dark bg-main focus:outline-none transition ease-in-out duration-150">
+                            <button
+                                class="inline-flex items-center px-3 py-2 border border-transparent text-xl leading-4 font-medium rounded-md text-dark bg-main focus:outline-none transition ease-in-out duration-150">
                                 <div>{{ Auth::user()->name }}</div>
 
                                 <div class="ms-1">
-                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                        viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd"
+                                            d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                            clip-rule="evenodd" />
                                     </svg>
                                 </div>
                             </button>
@@ -75,7 +67,8 @@
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
 
-                                <x-dropdown-link class="bg-main" :href="route('logout')" onclick="event.preventDefault();
+                                <x-dropdown-link class="bg-main" :href="route('logout')"
+                                    onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                                     {{ __('Log Out') }}
                                 </x-dropdown-link>
@@ -86,10 +79,15 @@
 
                 <!-- Hamburger -->
                 <div class="-me-2 flex items-center sm:hidden">
-                    <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-dark hover:text-dark-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                    <button @click="open = ! open"
+                        class="inline-flex items-center justify-center p-2 rounded-md text-dark hover:text-dark-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
                         <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-                            <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                            <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+                            <path :class="{ 'hidden': open, 'inline-flex': !open }" class="inline-flex"
+                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h16M4 18h16" />
+                            <path :class="{ 'hidden': !open, 'inline-flex': open }" class="hidden"
+                                stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12" />
                         </svg>
                     </button>
                 </div>
@@ -97,7 +95,7 @@
         </div>
 
         <!-- Responsive Navigation Menu -->
-        <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
+        <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
             <div class="pt-2 pb-3 space-y-1">
                 <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                     {{ __('Dashboard') }}
@@ -123,11 +121,13 @@
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
 
-                        <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault();
+                        <x-responsive-nav-link :href="route('logout')"
+                            onclick="event.preventDefault();
                                         this.closest('form').submit();">
                             {{ __('Log Out') }}
                         </x-responsive-nav-link>
                     </form>
+                    @endif
                 </div>
             </div>
         </div>
