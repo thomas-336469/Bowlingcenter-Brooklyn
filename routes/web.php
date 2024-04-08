a6f953358281a7858f96ab0be09eecbb83c6e201
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-04-08 10:38:21 +0200

    Add MazinReservationController with index method

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Controllers\AdminOptionsController;
+use App\Http\Controllers\MazinReservationController;
 use Illuminate\Routing\Middleware;
 use App\Http\Controllers\UserReservationController;
 use App\Http\Controllers\WorkerReservationController;
@@ -26,6 +27,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/reservations/$reservation/{id}update', [UserReservationController::class, 'update'])->name('reservations.update');
     Route::delete('/reservations/$reservation/{id}/delete', [UserReservationController::class, 'destroy'])->name('reservations.destroy');
     Route::post('/reservations/store', [UserReservationController::class, 'store'])->name('reservations.store');
+    Route::get('/mazinReservation', [MazinReservationController::class, 'index'])->name('mazinReservation.index');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
