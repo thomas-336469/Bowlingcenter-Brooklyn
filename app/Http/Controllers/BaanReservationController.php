7f3330118ec80456f0d84b73c16d8b188ab5fc0a
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-04-08 13:33:36 +0200

    Add edit and update methods to BaanReservationController

--- app/Http/Controllers/BaanReservationController.php
+++ app/Http/Controllers/BaanReservationController.php
@@ -14,4 +14,17 @@ class BaanReservationController extends Controller
         $mazinReservations = MazinReservation::all();
         return view('baanReservation.index', compact('mazinReservations', 'user'));
     }
+
+    public function edit($id)
+    {
+        $mazinReservation = MazinReservation::find($id);
+        return view('baanReservation.edit', compact('mazinReservation'));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $mazinReservation = MazinReservation::find($id);
+        $mazinReservation->update($request->all());
+        return redirect()->route('baanReservation.index')->with('success', 'Reservation updated successfully');
+    }
 }
