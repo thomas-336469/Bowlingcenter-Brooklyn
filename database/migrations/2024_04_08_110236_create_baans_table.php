43afbd05e470ec0386f6328fd370d8d2e62f1c5c
BolleTito335409 <chemichaeldekraker@gmail.com>
2024-04-08 13:06:51 +0200

    Create migration for 'baans' table

---
+++ database/migrations/2024_04_08_110236_create_baans_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Ramsey\Uuid\Type\Integer;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('baans', function (Blueprint $table) {
+            $table->id();
+            $table->Integer('Nummer');
+            $table->boolean('HeeftHek');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('baans');
+    }
+};
